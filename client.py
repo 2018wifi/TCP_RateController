ce061c25cf0bb48913e227ca7e147acbc721818e
Photooon <1527842029@qq.com>
2020-04-10 16:51:25 +0800

    udp socket

--- client.py
+++ client.py
@@ -1,19 +1,42 @@
 import time
 import socket
 
-rate = 45  # 发包速率，单位：包/秒
-server_ip = "192.168.0.105"
+RATE = 50  # 发包速率，单位：包/秒
+SERVER_IP = "192.168.0.105"
+PORT = 8000
+BYTE_NUM = 10   # 发送内容的字节长度(0...1472)
 
-def main():
+def udp():
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    mes = ""                            # 构造一定字节的内容
+    for i in range(0, BYTE_NUM + 1):
+        mes += str(i)
+
+    ts = int(time.time() * 1000)
+    interval = 1000 / float(RATE)
+    print("Initial ts:", ts)
+    print("Interval: ", interval, "ms")
+
+    while True:
+        if int(time.time() * 1000) < ts + interval:
+            continue
+        else:
+            ts += interval
+
+        udp_socket.sendto(mes.encode(), (SERVER_IP, 8000))
+        print("Have sent a packet to ", SERVER_IP, "\tts: ", int(time.time() * 1000))
+
+def tcpip():
     tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_ip = server_ip
-    tcp_port = 9000
+    tcp_ip = SERVER_IP
+    tcp_port = PORT
     tcp_socket.connect((tcp_ip, tcp_port))
 
     mes = "Something will be sent to server."
 
     ts = int(time.time() * 1000)
-    interval = 1000 / float(rate)
+    interval = 1000 / float(RATE)
     print("Initial ts:", ts)
     print("Interval: ", interval, "ms")
 
@@ -25,8 +48,8 @@ def main():
 
         tcp_socket.send(mes.encode())
         reply = tcp_socket.recv(4096)
-        print("Have sent a packet to ", server_ip, "\tReply: ", reply.decode(), "ts: ", int(time.time() * 1000))
+        print("Have sent a packet to ", SERVER_IP, "\tReply: ", reply.decode(), "ts: ", int(time.time() * 1000))
     # tcp_socket.close()
 
 if __name__ == '__main__':
-    main()
+    udp()
--- server.py
+++ server.py
@@ -1,13 +1,25 @@
 import socket
 
-def main():
+CLIENT_IP = "192.168.0.100"
+PORT = 8000
+
+def udp():
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind(("", PORT))
+
+    while True:
+        mes = udp_socket.recv(2048)
+        print(mes.decode())
+
+def tcpip():
     tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_socket.bind(("", 9000))
+    tcp_socket.bind(("", PORT))
     tcp_socket.listen(128)
     print("Listening...")
     reply = "Server Received!"
 
     client_socket, client_addr = tcp_socket.accept()
+
     while True:
         mes = client_socket.recv(4096)
         print("Received a packet from ", client_addr, "\tmessage len: ", len(mes))
@@ -15,5 +27,6 @@ def main():
         # client_socket.close()
     # tcp_socket.close()
 
+
 if __name__ == "__main__":
-    main()
+    udp()
